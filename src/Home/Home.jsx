75f9a900b686698fd9405ddc37e225ce92345b19
Santiagotamayoz <tamayozuluagas>
2023-10-25 12:23:52 -0500

    ok

---
+++ src/Home/Home.jsx
@@ -0,0 +1,16 @@
+import './Home.css'
+import { Menu } from '../Menu/Menu'
+export function Home(){
+    return(
+    <>
+    <header>
+        <menu></menu>
+    </header>
+    <section className='banner'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi corrupti, expedita sed atque optio pariatur rempossimus officia, 
+            blanditiis exercitationem maxime ducimus sint dicta eligendi id magni? Incidunt, quibusdam iure?
+        </p>
+    </section>
+    </>
+    )
+}
